ab75823b1dc86cf0c1cc4e09c3a7741ef25bd1c4
Shaul Kimel <shaullk600@gmail.com>
2025-09-08 16:51:04 +0300

    update: Progress in the controller and service phase - Opening documentation for the server - readme file

--- server/controllers/PostsCTRL.js
+++ server/controllers/PostsCTRL.js
@@ -1,10 +1,17 @@
 import express from "express";
 //services:
-import {  } from "../services/";
+import { readPosts } from "../services/PostsServise.js";
 
 
 
-export function readPosts(req,res){
-    const data = 
-
+export function readPosts(req, res) {
+    const res = readPosts();
+    if (res.status === 'ok') {
+        res.status(200).json(res.data);
+    }
+    else {
+        console.log(`Error log: Post Controller: \n An error occurred while retrieving data from a DB file \n Error type: Server`);
+        console.log('the message is: ', res.data);
+        res.sendStatus(502);
+    }
 }
--- server/services/PostsServise.js
+++ server/services/PostsServise.js
@@ -1,10 +1,17 @@
 import { readFileSync, writeFileSync } from 'fs';
+
 const urlDB = '../DB/Posts.tst'
 
-export function readAllPosts() {
-    const res = readFileSync(urlDB, { encoding: 'utf-8' });
-    const data = JSON.parse(res);
-    return data;
+export function readFileS() {
+    fs.readFile(urlDB, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+            return { success: 'failed', dataMessage: err };
+        }
+        if (data) {
+            return { success: 'success', dataMessage: data };
+
+        }
+    });
 }
 
 export function writeAllPosts(dataToWrite) {
@@ -12,3 +19,18 @@ export function writeAllPosts(dataToWrite) {
     const res = writeFileSync(urlDB, data, { encoding: 'utf-8' });
     return res;
 }
+
+// read:
+export function readPosts() {
+    const res = readFileS();
+    if (res.success === 'success') {
+        const data = JSON.parse(res.dataMessage);
+        return { status: 'ok', data: data };
+    }
+    else {
+        const err = res.dataMessage
+        return { status: 'failed', data: err };
+    }
+}
+
+
