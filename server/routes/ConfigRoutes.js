ddc5ff68f3950ac4c4e22f1baea7f30b3a318a05
Shaul Kimel <shaullk600@gmail.com>
2025-09-10 17:59:18 +0300

    update: Last commit before completion after the test

--- server/routes/ConfigRoutes.js
+++ server/routes/ConfigRoutes.js
@@ -1,31 +1,15 @@
 import { handlReadRequesting, handlReadRequestingPostById, handleCreateNewPost } from "../controllers/PostsCTRL.js";
+import { HandleUserRegistration, handleLoginUser } from "../controllers/UserCTRL.js";
 
 export default function ConfigRoutes(app) {
 
-    app.get('/read', (req, res, next) => {
-        console.log(`\nvalid: /read\n`);
-        next();
-    }, handlReadRequesting);
-
-    app.get('/read/:idPost', (req, res, next) => {
-        console.log(`\nvalid: /read/:idPost\n`);
-        next();
-    }, handlReadRequestingPostById);
-
-    app.post('/initPost/', (req, res, next) => {
-        console.log(`\n valid: /initPost/ \n`);
-        next();
-    }, handleCreateNewPost);
+    app.get('/read', handlReadRequesting);
+    app.get('/read/:idPost', handlReadRequestingPostById);
+    app.post('/initPost/', handleCreateNewPost);
 
     // login
-    app.post('/login', (req, res, next) => {
-        console.log(`\n valid: /login/ \n`);
-        next();
-    },  );
-    app.post('/register:idmanager', (req, res, next) => {
-        console.log(`\n valid: /register/ \n`);
-        next();
-    },  );
+    app.post('/login', handleLoginUser);
+    app.post('/register:idmanager', HandleUserRegistration);
 
 
     app.use('/', (req, res) => {
