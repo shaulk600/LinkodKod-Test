b46203b7d6d87e6179bb28accbc344f18f59a5d3
Shaul Kimel <shaullk600@gmail.com>
2025-09-10 16:44:30 +0300

    update: Login menu component executed successfully.

--- fronted/src/components/loginRegister/Register.tsx
+++ fronted/src/components/loginRegister/Register.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import "./Login.css";
+import "./Register.css";
 
 export default function Register() {
     const [userName, setUserName] = useState("");
