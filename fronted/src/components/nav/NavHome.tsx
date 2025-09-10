a154e170009a374b6cfe494cdf780ec3620f6b51
Shaul Kimel <shaullk600@gmail.com>
2025-09-10 12:40:40 +0300

    update file NavHome: adding button to search by URL and by ID

--- fronted/src/components/nav/NavHome.tsx
+++ fronted/src/components/nav/NavHome.tsx
@@ -5,7 +5,9 @@ import { Link } from "react-router";
 export default function NavHome() {
     return (
         <div id="compsNavHome">
-            <button > <Link to='/'> home </Link> </button>
+            <button className="btnNavHead"> <Link to='/'> home </Link> </button>
+            <button className="btnNavHead"> <Link to='/searchPost'> search Post by ID </Link> </button>
+            <button className="btnNavHead"> <Link to='/'> search Post by URL </Link> </button>
         </div>
     )
 }
