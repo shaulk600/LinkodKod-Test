dfdeae9ae0b1f78fdcae3710edf0b74595e74e95
Shaul Kimel <shaullk600@gmail.com>
2025-09-10 12:28:41 +0300

    adding: Adding a component with a home navigation button

---
+++ fronted/src/components/nav/NavHome.tsx
@@ -0,0 +1,11 @@
+import  "./NavHome.css";
+import { Link } from "react-router";
+
+
+export default function NavHome() {
+    return (
+        <div>
+            <button> <Link to='/'> home </Link> </button>
+        </div>
+    )
+}
